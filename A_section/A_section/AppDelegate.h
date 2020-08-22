59aa78aa9e8c1fc1ad52941204e960f4400566b5
sunjian <sunjian@sensetime.com>
2020-08-22 16:31:02 +0800

    Add A_section and .gitignore

---
+++ A_section/A_section/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  A_section
+//
+//  Created by sunjian on 2020/8/22.
+//  Copyright © 2020 sjuinan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
